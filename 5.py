6e213e0353cca63cd6e598e5f85a1a105990df83
庄欣熠 <2871832213@qq.com>
2023-10-12 16:49:13 +0800

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-def sort_print(num1,num2,num3):
-    nums=[num1,num2,num3]
-    sort_nums=sorted(nums)
-    print(sort_nums)
-    
-num1=float(input())
-num2=float(input())
-num3=float(input())
-sort_print(num1,num2,num3)
